8d3fb15e4250c90b569222da4ecd089efccd67e5
Karan Gorai <karangorai563@gmail.com>
2024-06-30 22:01:43 +0530

    counter added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+import Hi from "./hi"
+function App() {
+  return (
+    // <h1>Chai aur react vite</h1>
+    <Hi/>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/hi.jsx
@@ -0,0 +1,6 @@
+function Hi(){
+    return(
+        <h3>Helloo</h3>
+    )
+}
+export default Hi
