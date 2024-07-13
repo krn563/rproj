11c7d5b7050a898b7f540ae95c41484fbd8b49ae
Karan Gorai <karangorai563@gmail.com>
2024-07-13 23:24:51 +0530

    minor css fixes

--- Hooks/hook1useState/src/UseEffects/UseEffectAPI.jsx
+++ Hooks/hook1useState/src/UseEffects/UseEffectAPI.jsx
@@ -16,29 +16,29 @@ function UseEffectAPI() {
   return (
     <>
       <h2 className="text-center text-4xl">List of Github Users</h2>
-      <div className="grid items-center justify-center place-items-center grid-cols-3 mx-auto gap-5">
+      <div className="grid items-center justify-center mt-2 place-items-center grid-cols-3 mx-auto gap-5">
       {users.map((currEle) => {
         return (
           <div className="" key={currEle.id}>
-            <div className="flex bg-blue-400 w-fit p-4 rounded-md">
-              <div className="right bg-red-400 p-2 rounded-sm">
-                <img src={currEle.avatar_url} alt="image" className="w-40" />
+            <div className="flex bg-blue-100 w-fit p-4 rounded-md">
+              <div className="right p-2 rounded-sm">
+                <img src={currEle.avatar_url} alt="image" className="w-40 rounded-md" />
               </div>
-              <div className="left bg-lime-400 grid gap-1 p-2">
+              <div className="left  grid gap-1 p-2">
                 <div className="title text-3xl">{currEle.login}</div>
                 <div className="heading text-xl">Sub-Heading</div>
-                <div className="count flex gap-1">
-                  <div className="c1 bg-blue-600 border-2 p-2">
-                    <div className="articles">Articles</div>
-                    <div className="num1">num1</div>
+                <div className="count bg-blue-300 rounded-lg flex gap-1">
+                  <div className="c1 p-2">
+                    <div className="articles text-lg">Articles</div>
+                    <div className="num1 font-bold text-center">n1</div>
                   </div>
-                  <div className="c2 bg-blue-600 border-2 p-2">
-                    <div className="followers">Followers</div>
-                    <div className="num2">num2</div>
+                  <div className="c2 p-2">
+                    <div className="followers text-lg">Followers</div>
+                    <div className="num2 font-bold text-center">n2</div>
                   </div>
-                  <div className="c3 bg-blue-600 border-2 p-2">
-                    <div className="ratings">Ratings</div>
-                    <div className="num3">num3</div>
+                  <div className="c3 p-2">
+                    <div className="ratings text-lg">Ratings</div>
+                    <div className="num3 font-bold text-center">n3</div>
                   </div>
                 </div>
               </div>
